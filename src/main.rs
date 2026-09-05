6f6da3c9736750257a2037d9ee0dbabf5560a7ca
FireDasher <firelightpanda@gmail.com>
2026-09-05 07:52:01 -0700

    Change More Stuff

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,13 @@ use logos::Logos;
 
 use crate::token::Token;
 
-mod token;
 mod ast;
+mod token;
 
 fn main() {
-	let arg = std::env::args().nth(1).expect("You must provide and input file!");
+	let arg = std::env::args()
+		.nth(1)
+		.expect("You must provide and input file!");
 	let source = std::fs::read_to_string(arg).expect("File does not exist!");
 	let lex = Token::lexer(&source);
 
